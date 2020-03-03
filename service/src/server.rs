88d0bb054149d7857998c55dd89ed9a1937b5411
Charles Roussel <c.roussel@criteo.com>
2020-03-03 17:46:26 +0100

    Add configurable scheduler

--- service/src/server.rs
+++ service/src/server.rs
@@ -13,6 +13,7 @@ mod settings;
 mod knn;
 mod knn_controller;
 mod metric_observer;
+//mod health_check;
 
 use std::path::PathBuf;
 use failure::Error;
@@ -29,6 +30,7 @@ use metrics_runtime::Receiver;
 use clokwerk::{Scheduler, Interval};
 use metrics_core::Observe;
 use hnsw_rs::knnservice::Model;
+use tokio::runtime;
 
 
 struct ResultArgs {
@@ -36,19 +38,23 @@ struct ResultArgs {
     index_path: PathBuf,
     extra_item_path: PathBuf,
     models_path: Option<PathBuf>,
-    model: Model
+    model: Model,
+    core_count: Option<usize>
 }
 
 fn parse_args() -> Result<ResultArgs, Error> {
     let matches = App::new("Knn Serving")
         .arg(Arg::with_name("port").short("p").long("port").value_name("PORT").takes_value(true).required(true))
-        .arg(Arg::with_name("index_path").short("i").long("index_path").value_name("PATH").takes_value(true).required(true))
-        .arg(Arg::with_name("extra_path").short("e").long("extra_path").value_name("PATH").takes_value(true).required(true))
-        .arg(Arg::with_name("models_path").short("m").long("models_path").value_name("PATH").takes_value(true))
-        .arg(Arg::with_name("model").short("m").long("model").value_name("MODEL_NAME").required(true).takes_value(true))
+        .arg(Arg::with_name("index_path").short("ip").long("index_path").value_name("PATH").takes_value(true).required(true))
+        .arg(Arg::with_name("extra_path").short("ep").long("extra_path").value_name("PATH").takes_value(true).required(true))
+        .arg(Arg::with_name("models_path").short("mp").long("models_path").value_name("PATH").takes_value(true))
+        .arg(Arg::with_name("model").long("model").value_name("MODEL_NAME").required(true).takes_value(true))
+        .arg(Arg::with_name("core").short("c").long("core").value_name("CORE_COUNT").takes_value(true))
         .get_matches();
 
     let port: u16 = matches.value_of("port").unwrap().parse()?;
+    let core_count: Option<usize> = matches.value_of("core")
+        .and_then(|v| v.parse().ok());
     let index_path: PathBuf = PathBuf::from(shellexpand::tilde(matches.value_of("index_path").unwrap()).to_string());
     let extra_item_path: PathBuf = PathBuf::from(shellexpand::tilde(matches.value_of("extra_path").unwrap()).to_string());
     let models_path = matches.value_of("models_path").map(|m| PathBuf::from(shellexpand::tilde(m).to_string()));
@@ -58,12 +64,12 @@ fn parse_args() -> Result<ResultArgs, Error> {
         index_path,
         extra_item_path,
         models_path,
-        model
+        model,
+        core_count
     })
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>>{
+async fn run(settings: Settings, result_args: ResultArgs) -> Result<(), Box<dyn std::error::Error>> {
     env_logger::from_env(Env::default().default_filter_or("info")).init();
     let settings = Settings::new()?;
     let app_metrics = AtomicBucket::new();
@@ -92,7 +98,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     scheduler.every(Interval::Seconds(10)).run(move || controller.observe(&mut observer));
 
     let handler = scheduler.watch_thread(Duration::from_millis(500));
-    let result_args = parse_args()?;
+
     let addr = format!("0.0.0.0:{}", result_args.port).parse().unwrap();
     info!("Initializing server");
 
@@ -108,4 +114,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>{
     handler.stop();
     info!("Stopping the server");
     Ok(())
+}
+
+fn main() {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+    let settings = Settings::new().expect("Unable to parse settings");
+    let result_args = parse_args().expect("Unable to parse cli arguments");
+
+    let mut rt_builder = runtime::Builder::new();
+    rt_builder.threaded_scheduler();
+    if let Some(core_count) = &result_args.core_count {
+        rt_builder.core_threads(*core_count);
+    }
+    let mut rt = rt_builder.build().expect("Unable to initialize scheduler");
+    rt.block_on(run(settings, result_args)).expect("Unknown error");
 }
